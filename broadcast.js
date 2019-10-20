3f10a9a52ad04522fda88dd9afb146b45b370d69
webdeskme <atelford@webdesk.me>
2019-10-19 22:02:19 -0400

    Fleshed out broadcast

--- broadcast.js
+++ broadcast.js
@@ -4,7 +4,24 @@ $(document).ready(function() {
   var fs = require('fs');
   var os = require('os');
   var ifaces = os.networkInterfaces();
-
+  const {shell} = require('electron');
+  const wd_homedir = require('os').homedir();
+  var wd_dir = wd_homedir + '/Documents' + '/wdOS/WWW/';
+  var wd_home = wd_homedir + '/Documents/wdOS/';
+  var file = fs.readFileSync(wd_home + 'Core/broad.json');
+  var broad = JSON.parse(file);
+  $("#serv option[value=" + broad.serv + "]").prop('selected', true);
+  $("#port").val(broad.port);
+  $("#submit").click(function(){
+    var con = '{"serv": "';
+    con += $("#serv").val();
+    con += '", "port": ';
+    con += Math.max($("#port").val(), 80);
+    con += '}';
+    //con = JSON.stringify(con);
+	   fs.writeFileSync(wd_home + 'Core/broad.json', con);
+	    alert("Status: Saved \n Note: Restart WebDektop to see your changes.");
+  });
   Object.keys(ifaces).forEach(function (ifname) {
   var alias = 0;
 
@@ -16,14 +33,21 @@ $(document).ready(function() {
   var myIP = "";
   if (alias >= 1) {
   // this single interface has multiple ipv4 addresses
-  myIP += '<p><b>Go to: </b><u><i><a href="http://' + iface.address + ':8080" target="_blank">' + iface.address + ':8080</a></i></u> in your web browser.</p>';
+  myIP += '<p><b>Go to: </b><u><i><a href="http://' + iface.address + ':' + broad.port + '">' + iface.address + ':' + broad.port + '</a></i></u> in your web browser.</p>';
   } else {
   // this interface has only one ipv4 adress
-  myIP += '<p><b>Go to: </b><u><i><a href="http://' + iface.address + ':8080" target="_blank">' + iface.address + ':8080</a></i></u> in your web browser.</p>';
+  myIP += '<p><b>Go to: </b><u><i><a href="http://' + iface.address + ':' + broad.port + '">' + iface.address + ':' + broad.port + '</a></i></u> in your web browser.</p>';
   }
   ++alias;
   document.getElementById("myip").innerHTML = myIP;
   });
   //document.getElementById("myip").innerHTML = myIP;
   });
+  $("#wd_dir").click(function(){
+    shell.openItem(wd_dir);
+  });
+  $(document).on('click', 'a[href^="http"]', function(event) {
+    event.preventDefault();
+    shell.openExternal(this.href);
+});
 });
--- server.js
+++ server.js
@@ -5,8 +5,20 @@ $(document).ready(function() {
   const {shell} = require('electron');
   const wd_homedir = require('os').homedir();
   var wd_dir = wd_homedir + '/Documents' + '/wdOS/WWW/';
+  if (fs.existsSync(wd_homedir + '/Documents/wdOS/Core/broad.json')) {
+    var file = fs.readFileSync(wd_homedir + '/Documents/wdOS/Core/broad.json');
+    var broad = JSON.parse(file);
+    var port = broad.port;
+    var serv = broad.serv;
+  }
+  else{
+    fs.writeFileSync(wd_homedir + '/Documents/wdOS/Core/broad.json', '{"port": 8080, "serv": "on"}');
+    var port = "8080";
+    var serv = "on";
+  }
+  if(serv == "on"){
   //document.getElementById("files").innerHTML = '<h3>Site Map:</h3><ul>';
-  var con = '<!DOCTYPE html><html><title>CastFrame</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><body><h3>Site Map:</h3><ul>';
+  var con = '<!DOCTYPE html><html><title>WebDesktop</title><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"><body><h3>Site Map:</h3><ul>';
   files = fs.readdirSync(wd_dir);
   for (i = 0; i < files.length; i++) {
       if (fs.existsSync(wd_dir + files[i])) {
@@ -83,5 +95,6 @@ $(document).ready(function() {
       }
       }
       //myReadStream.pipe(res);
-  }).listen(8080);
+  }).listen(port);
+}
 });
