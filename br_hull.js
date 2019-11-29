065a9dd6ad3f2a0719bfe68408b7b9b377db45f7
webdeskme <atelford@webdesk.me>
2019-11-29 13:42:14 -0500

    install crypto-js

--- br_hull.js
+++ br_hull.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var SHA256 = require("crypto-js/sha256");
 const wd_homedir = require('os').homedir();
 var wd_home = wd_homedir + '/Documents/wdOS/Core/dh_hull/ac.json';
 $(document).ready(function(){
@@ -6,6 +7,7 @@ $(document).ready(function(){
 		var ac = $('#ac').val();
 		var user = $('#user').val();
 		var pwd = $('#pwd').val();
+		pwd = SHA256(pwd);
 		if (!fs.existsSync(wd_home)) {
       			var obj = {};
 			obj[ac] = {ac: ac, user: user, pwd: pwd};
@@ -20,9 +22,9 @@ $(document).ready(function(){
 	});
 	if(fs.existsSync(wd_home)){
 		var objl = JSON.parse(fs.readFileSync(wd_home, 'utf8'));
-		var con = "<ul>";
+		var con = '<ul class="list-group">';
 		for (var key in objl) {
-			con = con + '<li>' + key + '</li>';
+			con = con + '<li class="list-group-item">' + objl[key]['ac'] + ' => ' + objl[key]['user']  + '</li>';
 		}
 		con = con + '</ul>';
 		$('#list').html(con);
